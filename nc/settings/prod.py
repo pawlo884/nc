3c828ffbd3388e831921641d214270bee5e714de
Pawel <pawlo884@gmail.com>
2025-10-22 11:45:51 +0000

    fix: problem: Nginx nie mógł połączyć się z kontenerem Django (błąd 502) Przyczyna: Django odrzucał żądania z nginx, ponieważ web (alias kontenera w sieci Docker) nie był w ALLOWED_HOSTS Rozwiązanie: Dodałem 'web' do ALLOWED_HOSTS w /srv/app/nc/settings/prod.py

--- nc/settings/prod.py
+++ nc/settings/prod.py
@@ -14,6 +14,7 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
     '209.38.208.114',
     'app-web-1',  # Nazwa kontenera Docker
+    'web',  # Alias kontenera w sieci Docker
     # Dodaj konkretne domeny zamiast '*'
     # 'twoja-domena.com',
     # 'www.twoja-domena.com',
