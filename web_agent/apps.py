a9bd98f76e8c193fd6b9ec76b68b47b1dbcb7107
Paweł <pawlo884@gmail.com>
2025-11-23 18:36:14 +0100

    chore: remove all files from web_agent for restart

--- web_agent/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class WebAgentConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'web_agent'
-    verbose_name = 'Agent Web'
