cf11b40104341d69dcebd82e381f301102e91a3c
Paweł Sowa <pawlo884@gmail.com>
2025-12-03 16:29:36 +0100

    perf: optymalizacja db_routers - cache'owanie settings i DJANGO_ENV

--- nc/db_routers.py
+++ nc/db_routers.py
@@ -1,4 +1,9 @@
 import os
+from django.conf import settings
+
+# Cache'owanie wartości środowiska - wywoływane tylko raz przy starcie
+_DJANGO_ENV = os.getenv('DJANGO_ENV')
+_IS_PROD = _DJANGO_ENV == 'prod'
 
 
 class MPDRouter:
@@ -8,8 +13,7 @@ class MPDRouter:
 
     def db_for_read(self, model, **hints):
         if model._meta.app_label == 'MPD':
-            from django.conf import settings
-            if os.getenv('DJANGO_ENV') == 'prod' and 'MPD' in settings.DATABASES:
+            if _IS_PROD and 'MPD' in settings.DATABASES:
                 return 'MPD'
             if 'zzz_MPD' in settings.DATABASES:
                 return 'zzz_MPD'
@@ -17,8 +21,7 @@ class MPDRouter:
 
     def db_for_write(self, model, **hints):
         if model._meta.app_label == 'MPD':
-            from django.conf import settings
-            if os.getenv('DJANGO_ENV') == 'prod' and 'MPD' in settings.DATABASES:
+            if _IS_PROD and 'MPD' in settings.DATABASES:
                 return 'MPD'
             if 'zzz_MPD' in settings.DATABASES:
                 return 'zzz_MPD'
@@ -32,8 +35,7 @@ class MPDRouter:
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
         if app_label == 'MPD':
-            from django.conf import settings
-            if os.getenv('DJANGO_ENV') == 'prod' and 'MPD' in settings.DATABASES:
+            if _IS_PROD and 'MPD' in settings.DATABASES:
                 return db == 'MPD'
             if 'zzz_MPD' in settings.DATABASES:
                 return db == 'zzz_MPD'
@@ -48,9 +50,7 @@ class Matterhorn1Router:
     def db_for_read(self, model, **hints):
         if model._meta.app_label == 'matterhorn1':
             # Na produkcji używaj 'matterhorn1', lokalnie 'zzz_matterhorn1'
-            from django.conf import settings
-            django_env = os.getenv('DJANGO_ENV')
-            if django_env == 'prod' and 'matterhorn1' in settings.DATABASES:
+            if _IS_PROD and 'matterhorn1' in settings.DATABASES:
                 return 'matterhorn1'
             if 'zzz_matterhorn1' in settings.DATABASES:
                 return 'zzz_matterhorn1'
@@ -61,9 +61,7 @@ class Matterhorn1Router:
     def db_for_write(self, model, **hints):
         if model._meta.app_label == 'matterhorn1':
             # Na produkcji używaj 'matterhorn1', lokalnie 'zzz_matterhorn1'
-            from django.conf import settings
-            django_env = os.getenv('DJANGO_ENV')
-            if django_env == 'prod' and 'matterhorn1' in settings.DATABASES:
+            if _IS_PROD and 'matterhorn1' in settings.DATABASES:
                 return 'matterhorn1'
             if 'zzz_matterhorn1' in settings.DATABASES:
                 return 'zzz_matterhorn1'
@@ -78,9 +76,7 @@ class Matterhorn1Router:
     def allow_migrate(self, db, app_label, model_name=None, **hints):
         if app_label == 'matterhorn1':
             # Na produkcji używaj 'matterhorn1', lokalnie 'zzz_matterhorn1'
-            from django.conf import settings
-            django_env = os.getenv('DJANGO_ENV')
-            if django_env == 'prod' and 'matterhorn1' in settings.DATABASES:
+            if _IS_PROD and 'matterhorn1' in settings.DATABASES:
                 return db == 'matterhorn1'
             if 'zzz_matterhorn1' in settings.DATABASES:
                 return db == 'zzz_matterhorn1'
@@ -93,14 +89,16 @@ class DefaultRouter:
     """
     Router dla aplikacji systemowych Django
     """
+    # Lista aplikacji systemowych - tworzona tylko raz jako atrybut klasy
+    SYSTEM_APPS = frozenset([
+        'admin', 'auth', 'contenttypes', 'sessions', 'django_celery_beat',
+        'django_celery_results', 'admin_interface', 'colorfield'
+    ])
 
     def db_for_read(self, model, **hints):
         # Aplikacje systemowe Django idą do bazy default
-        system_apps = ['admin', 'auth', 'contenttypes', 'sessions', 'django_celery_beat',
-                       'django_celery_results', 'admin_interface', 'colorfield']
-        if model._meta.app_label in system_apps:
+        if model._meta.app_label in self.SYSTEM_APPS:
             # Na produkcji używaj 'default', lokalnie 'zzz_default'
-            from django.conf import settings
             if 'zzz_default' in settings.DATABASES:
                 return 'zzz_default'
             elif 'default' in settings.DATABASES:
@@ -109,11 +107,8 @@ class DefaultRouter:
 
     def db_for_write(self, model, **hints):
         # Aplikacje systemowe Django idą do bazy default
-        system_apps = ['admin', 'auth', 'contenttypes', 'sessions', 'django_celery_beat',
-                       'django_celery_results', 'admin_interface', 'colorfield']
-        if model._meta.app_label in system_apps:
+        if model._meta.app_label in self.SYSTEM_APPS:
             # Na produkcji używaj 'default', lokalnie 'zzz_default'
-            from django.conf import settings
             if 'zzz_default' in settings.DATABASES:
                 return 'zzz_default'
             elif 'default' in settings.DATABASES:
@@ -126,11 +121,8 @@ class DefaultRouter:
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
         # Aplikacje systemowe Django idą do bazy default
-        system_apps = ['admin', 'auth', 'contenttypes', 'sessions', 'django_celery_beat',
-                       'django_celery_results', 'admin_interface', 'colorfield']
-        if app_label in system_apps:
+        if app_label in self.SYSTEM_APPS:
             # Na produkcji używaj 'default', lokalnie 'zzz_default'
-            from django.conf import settings
             if 'zzz_default' in settings.DATABASES:
                 return db == 'zzz_default'
             elif 'default' in settings.DATABASES:
